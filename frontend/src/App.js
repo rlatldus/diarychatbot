8c29d31f40722078e4f9161b38d5b9a7181de93f
newExpand <magin0auto@gmail.com>
2023-06-21 01:04:01 +0900

    feat: 백엔드와 협의 후 소셜로그인용 라우트 작업

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Board from './pages/Board';
 import Main from './pages/Main';
 import ToasterProvider from './providers/ToasterProvider';
 import { checkAuthLoader } from './util/auth';
+import SocialLoginToken from './pages/SocialLoginToken';
 
 const router = createBrowserRouter([
     {
@@ -16,6 +17,10 @@ const router = createBrowserRouter([
                 path: 'main/:userId',
                 element: <Main />,
                 loader: checkAuthLoader,
+            },
+            {
+                path: 'loginRedirect/:userId',
+                element: <SocialLoginToken />,
             },
             { path: 'board', element: <Board /> },
         ],
