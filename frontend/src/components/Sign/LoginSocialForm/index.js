44325f9c32433e1ee3d7b3b4dc1fe5f902d39738
newExpand <magin0auto@gmail.com>
2023-06-25 00:33:21 +0900

    fix: 에러전까지 reset 및 소셜로그인 에러 수정

--- frontend/src/components/Sign/LoginSocialForm/index.js
+++ frontend/src/components/Sign/LoginSocialForm/index.js
@@ -2,26 +2,25 @@ import React from 'react';
 import Button from '../../@shared/Button';
 
 import * as Styled from './style';
-import { Link } from 'react-router-dom';
 
 const LoginSocialForm = () => {
     return (
         <Styled.StyleForm>
-            <Link to="https://jintakim.shop/login/google">
+            <a href="https://jintakim.shop/login/google">
                 <Button type="button" google>
                     google로 로그인
                 </Button>
-            </Link>
-            <Link to="https://jintakim.shop/login/kakao">
+            </a>
+            <a href="https://jintakim.shop/login/kakao">
                 <Button type="button" kakao>
                     카카오계정으로 로그인
                 </Button>
-            </Link>
-            <Link to="https://jintakim.shop/login/naver">
+            </a>
+            <a href="https://jintakim.shop/login/naver">
                 <Button type="button" naver>
                     네이버 아이디로 로그인
                 </Button>
-            </Link>
+            </a>
         </Styled.StyleForm>
     );
 };
