c481a6435d581d38487b612cbb9d4ae522548aad
newExpand <magin0auto@gmail.com>
2023-06-26 16:22:44 +0900

    move: 다이어리 관련 api 코드 diary.js로 이동

--- frontend/src/api/diary.js
+++ frontend/src/api/diary.js
@@ -20,3 +20,65 @@ export const getDiary = async (fetchDiary) => {
     });
     return response;
 };
+
+export const createDiary = async (formData) => {
+    const response = await instance.post('', {
+        query: `mutation{
+          createDiary(createChatInput:{
+            title: "${formData.title}",
+            ask: "${formData.ask}",
+            color: ${formData.color}
+          }){
+            id
+            title
+            ask
+            answer
+            score
+            color
+            user{
+             id
+            }
+            updatedAt
+          }
+        }`,
+    });
+    return response;
+};
+
+export const updateMyDiary = async (formData) => {
+    const response = await instance.post('', {
+        query: `mutation{
+          updateMyDiary(
+            id:"${formData.id}"
+            updateChatInput:{
+            title:"${formData.title}",
+            ask:"${formData.ask}",
+            color:${formData.color}
+          }){
+            id
+            title
+            ask
+            answer
+            score
+            color
+            user{
+                id
+            }
+            updatedAt
+            createdAt
+          }
+        }`,
+    });
+    return response;
+};
+
+export const deleteMyDiary = async (id) => {
+    const response = await instance.post('', {
+        query: `
+            mutation {
+              deleteMyDiary(
+                id: "${id}"
+              )} `,
+    });
+    return response;
+};
