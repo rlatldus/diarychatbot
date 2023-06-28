b58563a856406dfa2c63363d1deff5ead654953c
newExpand <magin0auto@gmail.com>
2023-06-28 16:49:54 +0900

    fix: 필터시 UI 흔들림 및 라우트 업데이트 렌더링 에러로 인한 수정

--- frontend/src/components/Main/BoardLists/index.js
+++ frontend/src/components/Main/BoardLists/index.js
@@ -1,10 +1,19 @@
+import { useNavigate, useParams } from 'react-router-dom';
 import BoardList from '../BoardList';
 
 import * as Styled from './style';
+import { useEffect } from 'react';
 
-const BoardLists = ({ fetchMyDiary, updatedAt }) => {
+const BoardLists = ({ fetchMyDiary, updatedAt, dataLoading }) => {
+    const navigate = useNavigate();
+    const { userId } = useParams();
+
+    useEffect(() => {
+        if (fetchMyDiary?.length === 0) return navigate(`/main/${userId}/Board`);
+    }, [fetchMyDiary?.length, navigate, userId]);
     return (
         <Styled.BoardWrapper>
+            {!fetchMyDiary && <div style={{ height: '100vh' }}></div>}
             {fetchMyDiary?.map((diary) => {
                 return (
                     <BoardList
